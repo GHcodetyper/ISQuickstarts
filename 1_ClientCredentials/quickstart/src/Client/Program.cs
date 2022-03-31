0961673ea7bd23ec30636fe5f62034dcf4c48274
VadB <vad_b@hotmail.com>
2022-03-31 20:52:14 +0300

    20220331 _1 Initial

---
+++ 1_ClientCredentials/quickstart/src/Client/Program.cs
@@ -0,0 +1,11 @@
+﻿using IdentityModel.Client;
+using System.Text.Json;
+
+// discover endpoints from metadata
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
