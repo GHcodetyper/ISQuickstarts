8a12ade95c264c2fc9c22a0d99834090df1bcca1
VadB <vad_b@hotmail.com>
2022-04-05 16:40:31 +0300

    20220405 _3 back

--- 2_InteractiveAspNetCore/quickstart/src/IdentityServer/Config.cs
+++ 2_InteractiveAspNetCore/quickstart/src/IdentityServer/Config.cs
@@ -1,13 +1,15 @@
 ﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer;
 
 namespace IdentityServer;
 
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
+        new List<IdentityResource>
         { 
-            new IdentityResources.OpenId()
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -24,6 +26,7 @@ public static class Config
                 ClientId = "client",
 
                 // no interactive user, use the clientid/secret for authentication
+                // machine to machine client (from quickstart 1)
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
 
                 // secret for authentication
@@ -34,6 +37,26 @@ public static class Config
 
                 // scopes that client has access to
                 AllowedScopes = { "api1" }
+            },
+            // interactive ASP.NET Core Web App
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256())},
+
+                AllowedGrantTypes = GrantTypes.Code,
+
+                // where to redirect to after login
+                RedirectUris = {"https://localhost:5002/signin-oidc"},
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = {"https://localhost:5002/signout-callback-oidc"},
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                }
             }
         };
 }
