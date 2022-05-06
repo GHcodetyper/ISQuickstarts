b5ea54f308553ffbf674b2bcd6938d077ac4afd0
VadB <vad_b@hotmail.com>
2022-05-06 18:39:39 +0300

    20220506 _4

--- 7_Blazor/src/Client/Program.cs
+++ 7_Blazor/src/Client/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using _7_Blazor.Client;
+using _7_Blazor.Client.BFF;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, BffAuthenticationStateProvider>();
 
 await builder.Build().RunAsync();
